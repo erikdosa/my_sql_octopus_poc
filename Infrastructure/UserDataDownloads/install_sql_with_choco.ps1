44b5f6d84ea62913b16cabd6c14b749a2c43d2c8
Alex Yates <Alex-Yates@users.noreply.github.com>
2020-09-25 16:06:43 +0100

    Update install_sql_with_choco.ps1

--- Infrastructure/UserDataDownloads/install_sql_with_choco.ps1
+++ Infrastructure/UserDataDownloads/install_sql_with_choco.ps1
@@ -1,7 +1,13 @@
+# updating the sa user
 Write-Host "Getting the name of the current user to replace in the copy ini file."
 $user = "$env:UserDomain\$env:USERNAME"
 write-host $user
 
+Write-Host "Replacing the placeholder user name in Confifuration.ini with $user."
+$replaceText = (Get-Content -path "C:\Startup\scripts\ConfigurationFile.ini" -Raw) -replace "__MY_USER__", $user
+Set-Content "C:\Startup\scripts\ConfigurationFile.ini" $replaceText
+
+# updating the sa password
 function get-secret(){
   param ($secret)
   $secretValue = Get-SECSecretValue -SecretId $secret
@@ -16,22 +22,22 @@ $saPassword = Get-Secret -secret "SQL_SA_PASSWORD"
 $replaceText = (Get-Content -path "C:\Startup\scripts\ConfigurationFile.ini" -Raw) -replace "__SQL_SA_PASSWORD__", $saPassword
 Set-Content "C:\Startup\scripts\ConfigurationFile.ini" $replaceText
 
-Write-Host "Replacing the placeholder user name in Confifuration.ini with $user."
-$replaceText = (Get-Content -path "C:\Startup\scripts\ConfigurationFile.ini" -Raw) -replace "__MY_USER__", $user
-Set-Content "C:\Startup\scripts\ConfigurationFile.ini" $replaceText
-
+# updating firewall
 Write-Host "Opening port 1433 on Windows Firewall"
 & netsh.exe firewall add portopening TCP 1433 "SQL Server"
 if ($lastExitCode -ne 0) {
   throw "Installation failed when modifying firewall rules"
 }
 
+# installng choco
 Write-Host "Installing Chocolatey."
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
 
+# installing SQL Server
 Write-Host "Installing SQL Server with Chocolatey."
 choco install sql-server-2019 --params="'/ConfigurationFile:C:\Startup\scripts\ConfigurationFile.ini'"
 
+# installng SSMS
 Write-Host "Also installing SSMS for convenience."
 choco install sql-server-management-studio
