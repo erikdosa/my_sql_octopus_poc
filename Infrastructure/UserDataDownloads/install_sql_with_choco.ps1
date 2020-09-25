b4bee4e5b76365e4807c9e7ec47e5a28dfdc4dfb
Alex Yates <alex.yates@dlmconsultants.com>
2020-09-25 14:31:30 +0100

    Adding SSMS to default SQL Server installer for convenience

--- Infrastructure/UserDataDownloads/install_sql_with_choco.ps1
+++ Infrastructure/UserDataDownloads/install_sql_with_choco.ps1
@@ -2,13 +2,16 @@ Write-Host "Getting the name of the current user to replace in the copy ini file
 $user = "$env:UserDomain\$env:USERNAME"
 write-host $user
 
-Write-Host "Replacing the placeholder user name with your username"
+Write-Host "Replacing the placeholder user name in Confifuration.ini with $user."
 $replaceText = (Get-Content -path "C:\Startup\scripts\ConfigurationFile.ini" -Raw) -replace "##MyUser##", $user
 Set-Content "C:\Startup\scripts\ConfigurationFile.ini" $replaceText
 
-Write-Host "Installing Chocolatey"
+Write-Host "Installing Chocolatey."
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-Write-Host "Installing SQL Server with Chocolatey"
 choco feature enable -n allowGlobalConfirmation
+
+Write-Host "Installing SQL Server with Chocolatey."
 choco install sql-server-2019 --params="'/ConfigurationFile:C:\Startup\scripts\ConfigurationFile.ini'"
+
+Write-Host "Also installing SSMS for convenience."
+choco install sql-server-management-studio
