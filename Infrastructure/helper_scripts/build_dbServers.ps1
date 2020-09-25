64b00bbe2f45ebf23f32d2593ca348d8a1ffc501
Alex Yates <alex.yates@dlmconsultants.com>
2020-09-25 19:56:43 +0100

    need to install NuGet before installing dbatools

--- Infrastructure/helper_scripts/build_dbServers.ps1
+++ Infrastructure/helper_scripts/build_dbServers.ps1
@@ -135,6 +135,11 @@ if ($Wait -and ($totalRequired -gt 0)){
     Write-Output "    While we wait for SQL Server to install, ensuring dbatools is installed on worker."
     Write-Output "      (We will use DBA tools to ping SQL Server to see when it comes online.)"
 
+    Write-Output "      Installing NuGet package provider (required for dbatools)..."
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force 
+    Write-Output "      Installing dbatools..."
+    Install-Module dbatools -Force
+
     if ($Installedmodules.name -contains "dbatools"){
         Write-Output "    Module dbatools is already installed "
     }
