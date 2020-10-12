8f9a861cd2dd2baa4efa1344f29df9cbe2a8baff
Alex Yates <alex.yates@dlmconsultants.com>
2020-10-12 19:20:53 +0100

    extracting all existing functions into a psm1

--- Infrastructure/helper_scripts/build_dbServers.ps1
+++ Infrastructure/helper_scripts/build_dbServers.ps1
@@ -10,6 +10,8 @@ param(
     $environment = "Manual run"
 )
 
+Import-Module -Name "$PSScriptRoot\functions.psm1" -Force
+
 $ErrorActionPreference = "Stop"
 $stopwatch =  [system.diagnostics.stopwatch]::StartNew()
 
@@ -78,73 +80,9 @@ $webServerRole = "$rolePrefix-WebServer"
 $dbServerRole = "$rolePrefix-DbServer"
 $dbJumpboxRole = "$rolePrefix-DbJumpbox"
 
-# Helper function to read and encoding the VM startup scripts
-Function Get-UserData {
-    param (
-        $fileName,
-        $role,
-        $sql_ip = "unknown"
-    )
-    
-    # retrieving raw source code
-    $userDataPath = "$PSScriptRoot\$filename"
-    if (-not (Test-Path $userDataPath)){
-        Write-Error "No UserData (VM startup script) found at $userDataPath!"
-    }
-    $userData = Get-Content -Path $userDataPath -Raw
-    
-    # replacing placeholder text
-    $userData = $userData.replace("__OCTOPUSURL__",$octoUrl)
-    $userData = $userData.replace("__ENV__",$tagValue)
-    $userData = $userData.replace("__ROLE__",$role)
-    $userData = $userData.replace("__SQLSERVERIP__",$sql_ip)
-
-    # Base 64 encoding the userdata file (required by EC2)
-    $encodedDbUserData = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($userData))
-
-    # returning encoded userdata
-    return $encodedDbUserData
-}
-
 # Reading and encoding the VM startup scripts
 $webServerUserData = Get-UserData -fileName "VM_UserData_WebServer.ps1" -role $webServerRole
 $dbServerUserData = Get-UserData -fileName "VM_UserData_DbServer.ps1" -role $dbServerRole
-
-# Helper function to get all the existing servers of a particular role
-Function Get-Servers {
-    param (
-        $role,
-        $value = $tagValue,
-        [switch]$includePending
-    )
-    $acceptableStates = "running"
-    if($includePending){
-        $acceptableStates = @("pending", "running")
-    }
-    $instances = (Get-EC2Instance -Filter @{Name="tag:$role";Values=$value}, @{Name="instance-state-name";Values=$acceptableStates}).Instances 
-    return $instances
-}
-
-# Helper function to build any servers that don't already exist
-Function Build-Servers {
-    param (
-        $role,
-        $value = $tagValue,
-        $encodedUserData,
-        $required = 1
-    )
-    $existingServers = Get-Servers -role $role -value $value -includePending
-    $required = $required - $existingServers.count
-    if ($required -gt 0){
-        $NewInstance = New-EC2Instance -ImageId $ami -MinCount $required -MaxCount $required -InstanceType $instanceType -UserData $encodedUserData -KeyName RandomQuotes_SQL -SecurityGroup RandomQuotes_SQL -IamInstanceProfile_Name RandomQuotes_SQL
-        # Tagging all the instances
-        ForEach ($InstanceID  in ($NewInstance.Instances).InstanceId){
-            New-EC2Tag -Resources $( $InstanceID ) -Tags @(
-                @{ Key=$role; Value=$value}
-            );
-        }
-    }    
-}
 
 Write-Output "    Checking infrastructure that's already running..."
 
@@ -371,54 +309,6 @@ Write-Output "        - Jumpbox tentacles: 270-330 seconds"
 Write-Output "        - Web server IIS installs: 350-400 seconds"
 Write-Output "        - Web server tentacles: 450-500 seconds"
 Write-Output "        - SQL Server install: 600-750 seconds"
-
-# Helper functions to ping the instances
-function Test-SQL {
-    param (
-        $ip,
-        $cred
-    )
-    try { 
-        Invoke-DbaQuery -SqlInstance $ip -Query 'SELECT @@version' -SqlCredential $cred -EnableException -QueryTimeout 1
-    }
-    catch {
-        return $false
-    }
-    return $true
-}
-
-function Test-IIS {
-    param (
-        $ip
-    )
-    try { 
-        $content = Invoke-WebRequest -Uri $ip -TimeoutSec 1 -UseBasicParsing
-    }
-    catch {
-        return $false
-    }
-    if ($content.toString() -like "*iisstart.png*"){
-    return $true
-    }
-}
-
-function Test-Tentacle {
-    param (
-        $ip
-    )
-    $header = @{ "X-Octopus-ApiKey" = $octoApiKey }
-    $uri = "https://" + $ip + ":10933/"
-    $environmentMachines = "/api/Spaces-1/environments/$envId/machines"
-    $machines = ((Invoke-WebRequest ($octoUrl + $environmentMachines) -Headers $header -UseBasicParsing).content | ConvertFrom-Json).items
-    $uriList = @()
-    $uriList += $machines.Uri 
-    if ($uriList -contains $uri){
-        return $true
-    }
-    else {
-        return $false
-    }
-}
 
 # Waiting to see if they all come online
 $allVmsConfigured = $false
---
+++ Infrastructure/helper_scripts/functions.psm1
@@ -0,0 +1,112 @@
+
+# Helper function to read and encoding the VM startup scripts
+Function Get-UserData {
+    param (
+        $fileName,
+        $role,
+        $sql_ip = "unknown"
+    )
+    
+    # retrieving raw source code
+    $userDataPath = "$PSScriptRoot\$filename"
+    if (-not (Test-Path $userDataPath)){
+        Write-Error "No UserData (VM startup script) found at $userDataPath!"
+    }
+    $userData = Get-Content -Path $userDataPath -Raw
+    
+    # replacing placeholder text
+    $userData = $userData.replace("__OCTOPUSURL__",$octoUrl)
+    $userData = $userData.replace("__ENV__",$tagValue)
+    $userData = $userData.replace("__ROLE__",$role)
+    $userData = $userData.replace("__SQLSERVERIP__",$sql_ip)
+
+    # Base 64 encoding the userdata file (required by EC2)
+    $encodedDbUserData = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($userData))
+
+    # returning encoded userdata
+    return $encodedDbUserData
+}
+
+# Helper function to get all the existing servers of a particular role
+Function Get-Servers {
+    param (
+        $role,
+        $value = $tagValue,
+        [switch]$includePending
+    )
+    $acceptableStates = "running"
+    if($includePending){
+        $acceptableStates = @("pending", "running")
+    }
+    $instances = (Get-EC2Instance -Filter @{Name="tag:$role";Values=$value}, @{Name="instance-state-name";Values=$acceptableStates}).Instances 
+    return $instances
+}
+
+# Helper function to build any servers that don't already exist
+Function Build-Servers {
+    param (
+        $role,
+        $value = $tagValue,
+        $encodedUserData,
+        $required = 1
+    )
+    $existingServers = Get-Servers -role $role -value $value -includePending
+    $required = $required - $existingServers.count
+    if ($required -gt 0){
+        $NewInstance = New-EC2Instance -ImageId $ami -MinCount $required -MaxCount $required -InstanceType $instanceType -UserData $encodedUserData -KeyName RandomQuotes_SQL -SecurityGroup RandomQuotes_SQL -IamInstanceProfile_Name RandomQuotes_SQL
+        # Tagging all the instances
+        ForEach ($InstanceID  in ($NewInstance.Instances).InstanceId){
+            New-EC2Tag -Resources $( $InstanceID ) -Tags @(
+                @{ Key=$role; Value=$value}
+            );
+        }
+    }    
+}
+
+# Helper functions to ping the instances
+function Test-SQL {
+    param (
+        $ip,
+        $cred
+    )
+    try { 
+        Invoke-DbaQuery -SqlInstance $ip -Query 'SELECT @@version' -SqlCredential $cred -EnableException -QueryTimeout 1
+    }
+    catch {
+        return $false
+    }
+    return $true
+}
+
+function Test-IIS {
+    param (
+        $ip
+    )
+    try { 
+        $content = Invoke-WebRequest -Uri $ip -TimeoutSec 1 -UseBasicParsing
+    }
+    catch {
+        return $false
+    }
+    if ($content.toString() -like "*iisstart.png*"){
+    return $true
+    }
+}
+
+function Test-Tentacle {
+    param (
+        $ip
+    )
+    $header = @{ "X-Octopus-ApiKey" = $octoApiKey }
+    $uri = "https://" + $ip + ":10933/"
+    $environmentMachines = "/api/Spaces-1/environments/$envId/machines"
+    $machines = ((Invoke-WebRequest ($octoUrl + $environmentMachines) -Headers $header -UseBasicParsing).content | ConvertFrom-Json).items
+    $uriList = @()
+    $uriList += $machines.Uri 
+    if ($uriList -contains $uri){
+        return $true
+    }
+    else {
+        return $false
+    }
+}
