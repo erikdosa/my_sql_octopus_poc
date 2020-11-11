850359c474b76c3f5bd7b8395dc98fe7e660ed65
Alex Yates <alex.yates@dlmconsultants.com>
2020-11-11 09:18:41 +0000

    simpler approach to starting a new session

--- Infrastructure/UserDataDownloads/install_jumpbox_ps_modules.ps1
+++ Infrastructure/UserDataDownloads/install_jumpbox_ps_modules.ps1
@@ -5,13 +5,11 @@ Install-Module PowerShellGet -MinimumVersion 1.6 -Force -AllowClobber
 Remove-Item -LiteralPath "C:\Program Files\WindowsPowerShell\Modules\PowerShellGet\1.0.0.1" -Force -Recurse
 
 # Creating a new session to install SqlChangeAutomation and SqlServer to ensure correct version of PowerShellGet is used
-$s = New-PSSession
-Write-Output "      Installing SQL Change Automation and SqlServer PowerShell modules..."
-$script = @"
+Invoke-Command { & "powershell.exe" } -NoNewScope
+
+Write-Output "      Installing SQL Change Automation PowerShell modules..."
 Install-Module SqlChangeAutomation -AcceptLicense -Force
 
+Write-Output "      Installing SqlServer PowerShell module..."
 # This bit is currently broken. Not sure why. Still trying to use the old PowerShellGet
-# Install-Module -Name SqlServer -AllowClobber -Force
-"@
-Invoke-Command -Session $s -ScriptBlock {$script}
-Remove-PSSession $s
+Install-Module -Name SqlServer -AllowClobber -Force
