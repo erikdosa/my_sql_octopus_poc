30cd45faae2885bdb4e45dc1bce5fb921476260e
Alex Yates <alex.yates@dlmconsultants.com>
2020-11-11 16:50:36 +0000

    correcting my use of separate powershell sessions

--- Infrastructure/UserDataDownloads/install_jumpbox_ps_modules.ps1
+++ Infrastructure/UserDataDownloads/install_jumpbox_ps_modules.ps1
@@ -4,16 +4,8 @@ Install-Module PowerShellGet -MinimumVersion 1.6 -Force -AllowClobber
 Write-Output "      Hard deleting the old version of PowerShellGet..."
 Remove-Item -LiteralPath "C:\Program Files\WindowsPowerShell\Modules\PowerShellGet\1.0.0.1" -Force -Recurse
 
-Write-Output "      Starting a new session to ensure using new version of PowerShellGet..."
-Invoke-Command { & "powershell.exe" } -NoNewScope
-
-$log = "C:\Startup\StartupLogSession2of2.txt"
-Write-Output " Creating a new log file at $log"
-Start-Transcript -path $log -append
-
 Write-Output "      Installing SQL Change Automation PowerShell module..."
-Install-Module SqlChangeAutomation -AcceptLicense -Force
+Invoke-Command { & "powershell.exe" 'Install-Module SqlChangeAutomation -AcceptLicense -Force' } -NoNewScope
 
 Write-Output "      Installing SqlServer PowerShell module..."
-# This bit is currently broken. Not sure why. Still trying to use the old PowerShellGet
-Install-Module -Name SqlServer -AllowClobber -Force
+Invoke-Command { & "powershell.exe" 'Install-Module -Name SqlServer -AllowClobber -Force' } -NoNewScope
