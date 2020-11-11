8d89f0ee3a1d1a74fa60742b69d84214b4c37743
Alex Yates <alex.yates@dlmconsultants.com>
2020-11-11 09:21:08 +0000

    Simpler way to start a new session

--- Infrastructure/UserDataDownloads/install_jumpbox_ps_modules.ps1
+++ Infrastructure/UserDataDownloads/install_jumpbox_ps_modules.ps1
@@ -1,13 +1,13 @@
 # Prerequisites
 Write-Output "      Installing new PowerShellGet (required for SQL Change Automation)..."
 Install-Module PowerShellGet -MinimumVersion 1.6 -Force -AllowClobber
-# Hard deleting the old version of PowerShellGet
+Write-Output "      Hard deleting the old version of PowerShellGet..."
 Remove-Item -LiteralPath "C:\Program Files\WindowsPowerShell\Modules\PowerShellGet\1.0.0.1" -Force -Recurse
 
-# Creating a new session to install SqlChangeAutomation and SqlServer to ensure correct version of PowerShellGet is used
+Write-Output "      Starting a new session to ensure using new version of PowerShellGet..."
 Invoke-Command { & "powershell.exe" } -NoNewScope
 
-Write-Output "      Installing SQL Change Automation PowerShell modules..."
+Write-Output "      Installing SQL Change Automation PowerShell module..."
 Install-Module SqlChangeAutomation -AcceptLicense -Force
 
 Write-Output "      Installing SqlServer PowerShell module..."
